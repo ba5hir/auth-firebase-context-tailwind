3a012b90796ab589e428c290996846a3f3aa4325
Bashir Ahamed <130704393+ba5hir@users.noreply.github.com>
2023-04-19 22:31:16 +0600

    Update AuthProviders.jsx

--- src/provider/AuthProviders.jsx
+++ src/provider/AuthProviders.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from "react";
 export const AuthContext = createContext(null);
 const AuthProviders = ({ children }) => {
   const user = {
-    displayName: "Bashir Ahamed",
+    displayName: "{}",
   };
   return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
 };
